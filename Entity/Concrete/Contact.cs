ec8039725245e93e23c4fb8f71bc1b7b0d4a1c3a
win <win@DESKTOP-QVNO1N8>
2021-09-07 22:02:38 +0300

    Add project files.

---
+++ Entity/Concrete/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete
+{
+   public class Contact
+    {
+        public int ContactID { get; set; }
+        public string ContactUserName { get; set; }
+        public string ContactMail { get; set; }
+        public string ContactSubject { get; set; }
+        public string ContactMessage { get; set; }
+        public string ContactDate { get; set; }
+        public string ContactStatus { get; set; }
+    }
+}
---
+++ Entity/Concrete/Writer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete
+{
+    public class Writer
+    {
+        public int WriteID { get; set; }
+        public string WriteName { get; set; }
+        public string WriteAbout { get; set; }
+        public string WriteImage { get; set; }
+        public string WriteMail { get; set; }
+        public string WritePassword { get; set; }
+        public bool WriteStatus { get; set; }
+    }
+}
